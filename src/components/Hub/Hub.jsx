7106a9c22cceb7a078dbe389981fc91f775914b1
Vera Prinsen <vera.prinsen@nedap.com>
2025-05-19 20:27:01 +0200

    Style the Hub

--- src/components/Hub/Hub.jsx
+++ src/components/Hub/Hub.jsx
@@ -1,10 +1,17 @@
 import React from "react";
+import { AiFillHome } from "react-icons/ai";
+import './Hub.scss'
 
-export const Hub = ({ children }) => {
+export const Hub = ({ title, handleClickHome, children }) => {
 	return (
-		<div>
-			<div>HUB</div>
-			{children}
+		<div className="main-container">
+			<div className="tool-hub">
+				<AiFillHome className="hub-icon" onClick={handleClickHome} />
+				<div className="hub-title">{title}</div>
+			</div>
+			<div className="tool-content">
+				{children}
+			</div>
 		</div>
 	)
 }
