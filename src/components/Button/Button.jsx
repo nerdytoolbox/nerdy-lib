cb73498d18e43016f4f07689c2d3c38d4ca60337
Vera Prinsen <vera.prinsen@nedap.com>
2025-05-26 21:59:23 +0200

    Add white button

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -7,12 +7,14 @@ export const Button = ({
 	size = 'default',
 	shape = 'rounded',
 	fill = 'filled',
-	color = 'gray',
+	color = 'white',
+	shade1 = "shade1",
+	shade2 = "shade9",
 	disabled,
 	selected
 }) => {
 	return (
-		<button className={`btn align-center ${size}-${shape} ${fill}-${color}${selected ? "-selected" : ""}`} disabled={disabled} onClick={onClick}>
+		<button className={`btn align-center ${size} ${shape} ${fill}-${color}-${shade1}-${shade2}${selected ? "-selected" : ""}`} disabled={disabled} onClick={onClick}>
 			{children}
 		</button>
 	);
