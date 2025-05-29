22ec507ef1520015287327b46e456ad332641a6e
Vera Prinsen <vera.prinsen@nedap.com>
2025-05-29 09:13:15 +0200

    Add arcs thumbnail

--- src/index.js
+++ src/index.js
@@ -2,6 +2,10 @@ import './index.scss'
 import './_mixins.scss'
 import './_colors.scss'
 
+import arcsThumbnail from './assets/arcs-thumbnail.png'
+
 export { Button } from './components/Button/Button'
 export { TextInput } from './components/TextInput/TextInput'
 export { Hub } from './components/Hub/Hub'
+
+export { arcsThumbnail };
