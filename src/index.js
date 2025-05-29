2e5451ed49a81f69c4e7a307a8cd1d8b14bc5bef
Vera Prinsen <vera.prinsen@nedap.com>
2025-05-29 11:53:39 +0200

    Change arcs icon

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import './index.scss'
 import './_mixins.scss'
 import './_colors.scss'
 
-import arcsThumbnail from './assets/arcs-thumbnail.png'
+import arcsThumbnail from './assets/arcsThumbnail.png'
 
 export { Button } from './components/Button/Button'
 export { TextInput } from './components/TextInput/TextInput'
