96a2bc37ca8f613f8ed2b9a59df49e082c348cf1
Vera Prinsen <vera.prinsen@nedap.com>
2025-06-04 11:18:57 +0200

    export image

--- src/index.js
+++ src/index.js
@@ -4,9 +4,10 @@ import './_colors.scss'
 
 import arcsThumbnail from './assets/arcsThumbnail.png'
 import witchOfFernIslandThumbnail from './assets/witchOfFernIslandThumbnail.png'
+import bggThumbnail from './assets/bggThumbnail.png'
 
 export { Button } from './components/Button/Button'
 export { TextInput } from './components/TextInput/TextInput'
 export { Hub } from './components/Hub/Hub'
 
-export { arcsThumbnail, witchOfFernIslandThumbnail };
+export { arcsThumbnail, witchOfFernIslandThumbnail, bggThumbnail };
