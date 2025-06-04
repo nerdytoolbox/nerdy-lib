7ad83efe2cb650d29269065193417210711ac4d7
Vera Prinsen <vera.prinsen@nedap.com>
2025-06-04 11:58:16 +0200

    Add value and onChange to TextInput

--- src/components/TextInput/TextInput.jsx
+++ src/components/TextInput/TextInput.jsx
@@ -1,9 +1,13 @@
 import React from "react";
 import './TextInput.scss';
 
-export const TextInput = ({ placeholder, extraClassNames = '' }) => {
+export const TextInput = ({
+	value = '',
+	onChange = () => {},
+  placeholder,
+  extraClassNames = '' }) => {
 	const classNames = `text-input ${extraClassNames}`
 	return (
-		<input className={classNames} placeholder={placeholder} />
+		<input className={classNames} value={value} placeholder={placeholder} onChange={onChange} />
 	)
 }
