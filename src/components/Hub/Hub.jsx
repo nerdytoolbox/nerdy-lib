af3c641e98702914fde90e25900bfc377eca5d13
Vera Prinsen <vera.prinsen@nedap.com>
2025-06-04 20:15:52 +0200

    remove images

--- src/components/Hub/Hub.jsx
+++ src/components/Hub/Hub.jsx
@@ -2,8 +2,13 @@ import React from "react";
 import { AiFillHome } from "react-icons/ai";
 import './Hub.scss'
 
-export const Hub = ({ title, children, footer, className }) => {
-
+export const Hub = ({
+  title,
+	icon,
+  children,
+  footer,
+  className
+}) => {
 	const handleClick = (e) => {
 		e.stopPropagation()
 		window.location.href = "/"
@@ -12,7 +17,8 @@ export const Hub = ({ title, children, footer, className }) => {
 	return (
 		<div className={`main-container ${className}`}>
 			<div className="tool-hub">
-				<AiFillHome className="hub-icon" onClick={handleClick} />
+				{icon && <img src={icon} alt="Home icon" className="custom-icon" onClick={handleClick} />}
+				{!icon && <AiFillHome className="hub-icon" onClick={handleClick} />}
 				<div className="hub-title">{title}</div>
 			</div>
 			<div className="tool-content">
--- src/index.js
+++ src/index.js
@@ -2,13 +2,6 @@ import './index.scss'
 import './_mixins.scss'
 import './_colors.scss'
 
-import arcsThumbnail from './assets/arcsThumbnail.png'
-import witchOfFernIslandThumbnail from './assets/witchOfFernIslandThumbnail.png'
-import bggThumbnail from './assets/bggThumbnail.png'
-import nerdyToolboxThumbnail from './assets/NerdyToolboxThumbnail.png'
-
 export { Button } from './components/Button/Button'
 export { TextInput } from './components/TextInput/TextInput'
 export { Hub } from './components/Hub/Hub'
-
-export { arcsThumbnail, witchOfFernIslandThumbnail, bggThumbnail, nerdyToolboxThumbnail };
