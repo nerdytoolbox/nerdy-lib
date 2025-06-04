ffb7462943110a1296d5603cc22719426339bd88
Vera Prinsen <vera.prinsen@nedap.com>
2025-06-04 16:40:38 +0200

    Add Nerdy Toolbox thumbnail

--- src/index.js
+++ src/index.js
@@ -5,9 +5,10 @@ import './_colors.scss'
 import arcsThumbnail from './assets/arcsThumbnail.png'
 import witchOfFernIslandThumbnail from './assets/witchOfFernIslandThumbnail.png'
 import bggThumbnail from './assets/bggThumbnail.png'
+import nerdyToolboxThumbnail from './assets/NerdyToolboxThumbnail.png'
 
 export { Button } from './components/Button/Button'
 export { TextInput } from './components/TextInput/TextInput'
 export { Hub } from './components/Hub/Hub'
 
-export { arcsThumbnail, witchOfFernIslandThumbnail, bggThumbnail };
+export { arcsThumbnail, witchOfFernIslandThumbnail, bggThumbnail, nerdyToolboxThumbnail };
