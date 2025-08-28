12ca474dccc4ef499d6b7c5cad276374d5d8e651
Vera Prinsen <vera.prinsen@nedap.com>
2025-08-28 15:12:48 +0200

    Add noIcon attribute to Title

--- src/components/Title/Title.jsx
+++ src/components/Title/Title.jsx
@@ -2,14 +2,14 @@ import React from "react"
 import logo from '../../assets/NerdyToolboxThumbnail.png'
 import './Title.scss'
 
-export const Title = ({ icon, text }) => {
+export const Title = ({ icon, text, noIcon = false }) => {
 	if (!icon) {
 		icon = logo
 	}
 
 	return (
 		<div className="title">
-			<img src={icon} alt={text} className="title-icon" />
+			{!noIcon && <img src={icon} alt={text} className="title-icon" />}
 			<div className="title-text">{text}</div>
 		</div>
 	)
